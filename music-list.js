303831d35c4ed5140e16c5a9ad4363f5d689344f
RustamDust <110399234+RustamDust@users.noreply.github.com>
2022-08-01 17:46:12 +0500

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+
+     {
+         name: " DarkSite ",
+         artist: " Alen Walker ",
+         img: "img1",
+         src: "song1"
+     },
+     {
+         name: " Qo'laringda Sinmoqdaman ",
+         artist: " Doxxim ",
+         img: "img2",
+         src: "song2"
+     },
+     {
+         name: " Destvo ",
+         artist: " Rauf & Faik ",
+         img: "img3",
+         src: "song3"
+     },
+     {
+         name: " Sevaman Hamon ",
+         artist: " Doxxim ",
+         img: "img4",
+         src: "song4"
+     },
+     {
+         name: " good 4 u ",
+         artist: " Olivia Rodrigo ",
+         img: "img5",
+         src: "song5"
+     },
+     {
+        name: "  Insoniyat ",
+        artist: " Doxxim ",
+        img: "img6",
+        src: "song6"
+    },
+    {
+        name: " Lovely ",
+        artist: " Billie Eilish, Khalid ",
+        img: "img7",
+        src: "song7"
+    },
+    {
+        name: " Major ",
+        artist: " Green71 ",
+        img: "img8",
+        src: "song8"
+    },
+    {
+        name: " Kechir Gulim ",
+        artist: " Doxxim ",
+        img: "img9",
+        src: "song9"
+    },
+    {
+        name: " Qarg'alar ",
+        artist: " Alyor ",
+        img: "img10",
+        src: "song10"
+    },
+     ];
